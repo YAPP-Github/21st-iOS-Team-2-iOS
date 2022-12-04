e740e29fb00840fea2f7ae22d49f54c836e3eefb
EthanPark <ethanpark604@gmail.com>
2022-12-05 01:12:50 +0900

    ✨ [feat] ViewModel 예시코드 작성

--- Fitfty/Projects/Auth/Sources/AuthViewModel.swift
+++ Fitfty/Projects/Auth/Sources/AuthViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  AuthViewModelState.swift
+//  AuthViewModel.swift
 //  Auth
 //
 //  Created by Watcha-Ethan on 2022/12/05.
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import Combine
+import Common
+
+final public class AuthViewModel: ViewModelType {
+    public enum ViewModelState {
+        case presentKakaoLoginView
+        case pushOnboardingView
+        case doSomething
+    }
+    
+    public var state: PassthroughSubject<ViewModelState, Never> = .init()
+    
+    public init() {}
+    
+    func didTapKakaoLogin() {
+        state.send(.presentKakaoLoginView)
+    }
+    
+    func requestKakaoLogin() {
+        // request API... Success !
+        state.send(.pushOnboardingView)
+    }
+}
