632a7688a4c872b9ff44fcc5a1af01c42d14cb9b
Ari <lee_ari95@icloud.com>
2022-12-06 18:35:21 +0900

    ✨ [feat] 날씨 정보를 나타내는 뷰와 날씨 아이콘을 나타내는 Cell 구현

---
+++ Fitfty/Projects/MainFeed/Sources/Views/WeatherInfoView.swift
@@ -0,0 +1,69 @@
+//
+//  WeatherInfoView.swift
+//  MainFeed
+//
+//  Created by Ari on 2022/12/05.
+//  Copyright © 2022 Fitfty. All rights reserved.
+//
+
+import UIKit
+
+final class WeatherInfoView: UIStackView {
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: -1.0, height: 80)
+    }
+
+    lazy var backgroundStackView: UIStackView = {
+        let stackView = UIStackView(axis: .horizontal, alignment: .top, distribution: .fill, spacing: 8)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.backgroundColor = .clear
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0)
+        stackView.addArrangedSubviews(conditionLabel, tempInfoLabel)
+        return stackView
+    }()
+
+    private lazy var tempLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 54, weight: .bold)
+        label.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        return label
+    }()
+
+    private lazy var conditionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGray
+        label.font = .preferredFont(for: .callout, weight: .regular)
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return label
+    }()
+
+    private lazy var tempInfoLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .preferredFont(for: .callout, weight: .regular)
+        label.setContentHuggingPriority(.required, for: .horizontal)
+        return label
+    }()
+    
+    convenience init(temp: Int, condition: String, minimum: Int, maximum: Int) {
+        self.init(frame: .zero)
+        configure()
+        tempLabel.text = "\(temp)°"
+        conditionLabel.text = condition
+        tempInfoLabel.text = "최저\(minimum) ㆍ 최고\(maximum)"
+    }
+    
+    private func configure() {
+        axis = .horizontal
+        alignment = .top
+        distribution = .fill
+        spacing = 8
+        backgroundColor = .clear
+        isLayoutMarginsRelativeArrangement = true
+        directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+        addArrangedSubviews(tempLabel, backgroundStackView)
+    }
+    
+}
