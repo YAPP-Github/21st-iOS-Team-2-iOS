6c7719addb9d6902036f80c11cf38e419cf86118
Ari <lee_ari95@icloud.com>
2022-12-06 18:39:44 +0900

    ✨ [feat] FooterView와 HeaderView를 추가

---
+++ Fitfty/Projects/MainFeed/Sources/Views/FooterView.swift
@@ -0,0 +1,21 @@
+//
+//  FooterView.swift
+//  MainFeed
+//
+//  Created by Ari on 2022/12/05.
+//  Copyright © 2022 Fitfty. All rights reserved.
+//
+
+import UIKit
+
+class FooterView: UICollectionReusableView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemGray6
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
