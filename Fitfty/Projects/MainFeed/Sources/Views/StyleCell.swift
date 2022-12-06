f3a3851d3b3c2786ec4e5fca8285a3fd16d5352a
Ari <lee_ari95@icloud.com>
2022-12-07 00:19:16 +0900

    🔧 [chore] 코드 앞 공백 맞지 않는 부분 개선

--- Fitfty/Projects/MainFeed/Sources/Views/StyleCell.swift
+++ Fitfty/Projects/MainFeed/Sources/Views/StyleCell.swift
@@ -46,12 +46,12 @@ final class StyleCell: UICollectionViewCell {
     }
     
     override func systemLayoutSizeFitting(
-            _ targetSize: CGSize,
-            withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority,
-            verticalFittingPriority: UILayoutPriority
-        ) -> CGSize {
-            return CGSize(width: intrinsicContentSize.width, height: intrinsicContentSize.height)
-        }
+        _ targetSize: CGSize,
+        withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority,
+        verticalFittingPriority: UILayoutPriority
+    ) -> CGSize {
+        return CGSize(width: intrinsicContentSize.width, height: intrinsicContentSize.height)
+    }
     
     private func configure() {
         contentView.addSubviews(titleLabel)
