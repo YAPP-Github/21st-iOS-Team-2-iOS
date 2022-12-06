e085dcb64d961b0d0abf692feea99d300cbd70ac
Ari <lee_ari95@icloud.com>
2022-12-06 23:40:39 +0900

    ✨ [feat] CodyCell 내부에 프로필 정보 추가 구현

--- Fitfty/Projects/MainFeed/Sources/Views/CodyCell.swift
+++ Fitfty/Projects/MainFeed/Sources/Views/CodyCell.swift
@@ -15,7 +15,7 @@ final class CodyCell: UICollectionViewCell {
         return CGSize(width: 300, height: 336)
     }
     
-    private lazy var imageView: UIImageView = {
+    private lazy var codyImageView: UIImageView = {
         let image = UIImageView()
         image.layer.cornerRadius = 15
         image.clipsToBounds = true
@@ -27,6 +27,48 @@ final class CodyCell: UICollectionViewCell {
         gradientLayer.cornerRadius = 15
         image.layer.addSublayer(gradientLayer)
         return image
+    }()
+    
+    private lazy var profileStackView: UIStackView = {
+        let stackView = UIStackView(axis: .horizontal, alignment: .center, distribution: .fill, spacing: 8)
+        stackView.backgroundColor = .clear
+        stackView.addArrangedSubviews(profileImageView, profileInfoStackView)
+        return stackView
+    }()
+    
+    private lazy var profileImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = CommonAsset.Images.sample.image
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 20
+        image.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        image.heightAnchor.constraint(equalTo: image.widthAnchor).isActive = true
+        return image
+    }()
+    
+    private lazy var profileInfoStackView: UIStackView = {
+        let stackView = UIStackView(axis: .vertical, alignment: .leading, distribution: .fill, spacing: 2)
+        stackView.backgroundColor = .clear
+        stackView.addArrangedSubviews(nameLabel, dateLabel)
+        return stackView
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .preferredFont(for: .footnote, weight: .bold)
+        label.setContentHuggingPriority(.required, for: .horizontal)
+        label.text = "iloveiso2"
+        return label
+    }()
+    
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = CommonAsset.Colors.ftSecondaryText.color
+        label.font = .preferredFont(for: .footnote, weight: .semibold)
+        label.setContentHuggingPriority(.required, for: .horizontal)
+        label.text = "4시간"
+        return label
     }()
     
     override func prepareForReuse() {
@@ -52,23 +94,25 @@ final class CodyCell: UICollectionViewCell {
         }
     
     private func configure() {
-        contentView.addSubviews(imageView)
+        contentView.addSubviews(codyImageView, profileStackView)
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            codyImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            codyImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            codyImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            codyImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            profileStackView.leadingAnchor.constraint(equalTo: codyImageView.leadingAnchor, constant: 20),
+            profileStackView.bottomAnchor.constraint(equalTo: codyImageView.bottomAnchor, constant: -20)
         ])
     }
     
     private func reset() {
-        imageView.image = nil
+        codyImageView.image = nil
     }
     
 }
 
 extension CodyCell {
-    func setUp(text: String) {
+    func setUp() {
         
     }
 }
