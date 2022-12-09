a6227be27bab68091b0e469679b568f4e25e35bb
youngsun <dudtjs1021@hansung.ac.kr>
2022-12-09 23:32:38 +0900

    ✨ [feat] headerView 구현
    - 프로필을 보여주는 ProfileView 구현
    - 나의코디, 즐겨찾기 버튼이 있는 MenuView 구현

---
+++ Fitfty/Projects/Profile/Sources/View/ProfileView.swift
@@ -0,0 +1,119 @@
+//
+//  ProfileView.swift
+//  Profile
+//
+//  Created by 임영선 on 2022/12/09.
+//  Copyright © 2022 Fitfty. All rights reserved.
+//
+
+import UIKit
+import Common
+
+final class ProfileView: UIView {
+
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = CommonAsset.Images.profileSample.image
+        imageView.contentMode = .scaleToFill
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 52
+        return imageView
+    }()
+    
+    private lazy var nicknameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "iloveios2"
+        label.font = FitftyFont.SFProDisplaySemibold(size: 24).font
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var contentLabel: UILabel = {
+        let label = UILabel()
+        label.text = "안녕하세요! 반갑습니다"
+        label.font = FitftyFont.appleSDMedium(size: 15).font
+        label.textColor = UIColor(red: 0.512, green: 0.512, blue: 0.567, alpha: 1)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var followView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0.217, green: 0.671, blue: 1, alpha: 0.1)
+        view.layer.cornerRadius = 20
+        return view
+    }()
+
+    private lazy var followButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(UIColor(red: 0.078, green: 0.364, blue: 0.571, alpha: 1), for: .normal)
+        button.setTitle("팔로우 0", for: .normal)
+        button.titleLabel?.font = FitftyFont.appleSDSemiBold(size: 13).font
+        return button
+    }()
+    
+    private lazy var followerButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(UIColor(red: 0.078, green: 0.364, blue: 0.571, alpha: 1), for: .normal)
+        button.setTitle("팔로워 0", for: .normal)
+        button.titleLabel?.font = FitftyFont.appleSDSemiBold(size: 13).font
+        return button
+    }()
+    
+    private lazy var dotLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ㆍ"
+        label.textColor = UIColor(red: 0.227, green: 0.227, blue: 0.235, alpha: 1)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpConstraintLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpConstraintLayout() {
+        [imageView, nicknameLabel, contentLabel, followView].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        }
+        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 104).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 104).isActive = true
+        
+        nicknameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        
+        contentLabel.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: 6).isActive = true
+        
+        followView.topAnchor.constraint(equalTo: contentLabel.bottomAnchor, constant: 16).isActive = true
+        followView.widthAnchor.constraint(equalToConstant: 144).isActive = true
+        followView.heightAnchor.constraint(equalToConstant: 34).isActive = true
+        
+        [followButton, followerButton, dotLabel].forEach {
+            followView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        dotLabel.centerXAnchor.constraint(equalTo: followView.centerXAnchor).isActive = true
+        dotLabel.centerYAnchor.constraint(equalTo: followView.centerYAnchor).isActive = true
+        
+        followButton.rightAnchor.constraint(equalTo: dotLabel.leftAnchor, constant: -3).isActive = true
+        followButton.centerYAnchor.constraint(equalTo: followView.centerYAnchor).isActive = true
+        
+        followerButton.leftAnchor.constraint(equalTo: dotLabel.rightAnchor, constant: 3).isActive = true
+        followerButton.centerYAnchor.constraint(equalTo: followView.centerYAnchor).isActive = true
+    }
+}
+
+extension ProfileView {
+    func setUp(nickname: String, content: String, follow: Int, follower: Int) {
+        nicknameLabel.text = nickname
+        contentLabel.text = content
+        followButton.setTitle("팔로우 \(follow)", for: .normal)
+        followerButton.setTitle("팔로워 \(follower)", for: .normal)
+    }
+}
--- Fitfty/Projects/Profile/Sources/ViewController/ProfileViewController.swift
+++ Fitfty/Projects/Profile/Sources/ViewController/ProfileViewController.swift
@@ -7,15 +7,12 @@
 //
 
 import UIKit
-import Auth
 
 final public class ProfileViewController: UIViewController {
     
     enum Section: CaseIterable {
         case feed
     }
-    
-    public weak var coordinator: AuthCoordinatorInterface?
     
     private var dataSource: UICollectionViewDiffableDataSource<Section, UUID>?
     
@@ -31,7 +28,6 @@ final public class ProfileViewController: UIViewController {
         setUpDataSource()
         applySnapshot()
     }
-
 }
 
 private extension ProfileViewController {
@@ -48,6 +44,10 @@ private extension ProfileViewController {
         collectionView.delegate = self
         collectionView.register(FeedImageCell.self,
                                 forCellWithReuseIdentifier: FeedImageCell.identifier)
+        collectionView.register(HeaderView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: HeaderView.identifier)
+        
         dataSource = UICollectionViewDiffableDataSource<Section, UUID>(
             collectionView: collectionView,
             cellProvider: { (collectionView, indexPath, _) -> UICollectionViewCell? in
@@ -58,6 +58,17 @@ private extension ProfileViewController {
                 }
                 return cell
             })
+        
+        dataSource?.supplementaryViewProvider = { (collectionView, kind, indexPath) -> UICollectionReusableView? in
+            guard let supplementaryView = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: HeaderView.identifier,
+                for: indexPath) as? HeaderView else {
+                return UICollectionReusableView()
+            }
+            supplementaryView.profileView.setUp(nickname: "iosLover", content: "안녕하세용!", follow: 14, follower: 22)
+            return supplementaryView
+        }
     }
     
     func applySnapshot() {
@@ -97,6 +108,13 @@ extension ProfileViewController: UICollectionViewDelegateFlowLayout {
         layout collectionViewLayout: UICollectionViewLayout,
         insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 8, left: 20, bottom: 8, right: 20)
+    }
+    
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        referenceSizeForHeaderInSection section: Int) -> CGSize {
+            return CGSize(width: collectionView.frame.width, height: 400)
     }
     
 }
