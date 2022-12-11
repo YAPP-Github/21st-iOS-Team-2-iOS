0dc0ceb76e66f59ca22e7df4e27aaefe0bb7fa0a
EthanPark <ethanpark604@gmail.com>
2022-12-12 02:59:24 +0900

    ✨ [feat] WeatherAPI 작업

--- Fitfty/Projects/Core/Sources/API.swift
+++ Fitfty/Projects/Core/Sources/API.swift
@@ -1,5 +1,5 @@
 //
-//  Core.swift
+//  API.swift
 //  ProjectDescriptionHelpers
 //
 //  Created by Ari on 2022/12/02.
@@ -8,8 +8,88 @@
 import Foundation
 import Moya
 
-public enum OpenWeaterAPI {
-    
+// MARK: - WeatherAPI
+
+public enum WeatherAPI {
+    case fetchWeather(parameter: [String: Any])
+    case fetchPastWeather(parameter: [String: Any])
 }
 
+extension WeatherAPI: TargetType {
+    public var baseURL: URL {
+        return URL(string: "http://apis.data.go.kr/1360000")!
+    }
+    
+    public var path: String {
+        switch self {
+        case .fetchWeather: return "/VilageFcstInfoService_2.0/getUltraSrtNcst"
+        case .fetchPastWeather: return "/AsosHourlyInfoService/getWthrDataList"
+        }
+    }
+    
+    public var method: Moya.Method {
+        return .get
+    }
+    
+    public var task: Moya.Task {
+        switch self {
+        case .fetchWeather(let parameter), .fetchPastWeather(let parameter):
+            let parameter = updateParameters(parameter)
+            return .requestParameters(parameters: parameter, encoding: URLEncoding.queryString)
+        }
+    }
+    
+    public var headers: [String: String]? {
+        return nil
+    }
+}
 
+public extension WeatherAPI {
+    static func requestWeather<T: Decodable>(target: WeatherAPI, dataType: T.Type) async throws -> T {
+        return try await withCheckedThrowingContinuation { continuation in
+            let provider = MoyaProvider<WeatherAPI>()
+            provider.request(target) { result in
+                switch result {
+                case .success(let response):
+                    print("didFinishRequest URL [\(response.request?.url?.absoluteString ?? "")]")
+                    guard let data = try? JSONDecoder().decode(T.self, from: response.data) else {
+                        return
+                    }
+                    continuation.resume(returning: data)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    static func requestWeather(target: WeatherAPI) async throws -> Response {
+        return try await withCheckedThrowingContinuation { continuation in
+            let provider = MoyaProvider<WeatherAPI>()
+            provider.request(target) { result in
+                switch result {
+                case .success(let response):
+                    print("didFinishRequest URL [\(response.request?.url?.absoluteString ?? "")]")
+                    continuation.resume(returning: response)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - FitftyAPI
+
+public enum FitftyAPI {
+
+}
+
+private extension TargetType {
+    func updateParameters(_ parameter: [String: Any]) -> [String: Any] {
+        var parameter = parameter
+        parameter.updateValue(APIKey.apiKey.removingPercentEncoding ?? "", forKey: "serviceKey")
+        parameter.updateValue("json", forKey: "dataType")
+        return parameter
+    }
+}
