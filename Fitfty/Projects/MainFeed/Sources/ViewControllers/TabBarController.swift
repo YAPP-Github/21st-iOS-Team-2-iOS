d9cc34d3b5b0df4b19ad4586dd8be913f0fa065d
Ari <lee_ari95@icloud.com>
2022-12-11 14:17:22 +0900

    ✨ [feat] TabBar를 터치했을 때 아이콘이 변경되도록 추가
    - TabBarPage 타입을 TabarController 타입 내부로 이동

--- Fitfty/Projects/MainFeed/Sources/ViewControllers/TabBarController.swift
+++ Fitfty/Projects/MainFeed/Sources/ViewControllers/TabBarController.swift
@@ -7,8 +7,47 @@
 //
 
 import UIKit
+import Common
+
+public enum TabBarPage {
+    
+    case weather
+    case createCody
+    case profile
+    
+    public init?(index: Int) {
+        switch index {
+        case 0: self = .weather
+        case 1: self = .createCody
+        case 2: self = .profile
+        default: return nil
+        }
+    }
+    
+    public var pageOrderNumber: Int {
+        switch self {
+        case .weather: return 0
+        case .createCody: return 1
+        case .profile: return 2
+        }
+    }
+    
+    public var pageIconImage: UIImage? {
+        switch self {
+        case .weather: return CommonAsset.Images.iconSunFill.image.withRenderingMode(.alwaysOriginal)
+        case .createCody: return CommonAsset.Images.iconAdd.image.withRenderingMode(.alwaysOriginal)
+        case .profile: return CommonAsset.Images.iconProfile.image.withRenderingMode(.alwaysOriginal)
+        }
+    }
+    
+}
 
 public final class TabBarController: UITabBarController {
+    
+    private lazy var sunImage = CommonAsset.Images.iconSun.image.withRenderingMode(.alwaysOriginal)
+    private lazy var sunFillImage = CommonAsset.Images.iconSunFill.image.withRenderingMode(.alwaysOriginal)
+    private lazy var profileImage = CommonAsset.Images.iconProfile.image.withRenderingMode(.alwaysOriginal)
+    private lazy var profileFillImage = CommonAsset.Images.iconProfileFill.image.withRenderingMode(.alwaysOriginal)
     
     public init() {
         super.init(nibName: nil, bundle: nil)
@@ -25,4 +64,32 @@ public final class TabBarController: UITabBarController {
             return CGSize(width: UIScreen.main.bounds.width, height: 96)
         }
     }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+    }
+    
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    
+    public func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        guard let tabBar = TabBarPage(index: tabBarController.selectedIndex) else {
+            return
+        }
+        switch tabBar {
+        case .weather:
+            tabBarController.tabBar.items?[tabBar.pageOrderNumber].image = sunFillImage
+            tabBarController.tabBar.items?[TabBarPage.profile.pageOrderNumber].image = profileImage
+            
+        case .profile:
+            tabBarController.tabBar.items?[tabBar.pageOrderNumber].image = profileFillImage
+            tabBarController.tabBar.items?[TabBarPage.weather.pageOrderNumber].image = sunImage
+            
+        default:
+            return
+        }
+    }
+    
 }
