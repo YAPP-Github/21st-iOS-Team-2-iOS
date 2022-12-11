a4dea15bc59936f66cfa2b4d2f5b8142615ee7fa
EthanPark <ethanpark604@gmail.com>
2022-12-12 03:00:11 +0900

    ✅ [test] WeatherAPI 테스트 추가

--- Fitfty/Projects/Core/Tests/WeatherAPITest.swift
+++ Fitfty/Projects/Core/Tests/WeatherAPITest.swift
@@ -1,8 +1,58 @@
 //
-//  CoreTests.swift
+//  WeatherAPITest.swift
 //  ProjectDescriptionHelpers
 //
 //  Created by Ari on 2022/12/02.
 //
 
-import Foundation
+import XCTest
+@testable import Core
+
+final class WeatherAPITests: XCTestCase {
+    var todayDate: String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYYMMDD"
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    func testFetchWeatherAPI_statusCode가_200인지() async throws {
+        let parameter: [String: Any] = [
+            "numOfRows": "10",
+            "pageNo": "1",
+            "base_date": todayDate,
+            "base_time": "0000",
+            "nx": "55",
+            "ny": "127"
+        ]
+        
+        do {
+            let response = try await WeatherAPI.requestWeather(target: .fetchWeather(parameter: parameter))
+            XCTAssertEqual(response.statusCode, 200)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testPastFetchWeatherAPI_statusCode가_200인지() async throws {
+        let parameter: [String: Any] = [
+            "numOfRows": "10",
+            "pageNo": "1",
+            "dataCd": "ASOS",
+            "dateCd": "HR",
+            "stnIds": "108",
+            "startDt": todayDate,
+            "startHh": "00",
+            "endDt": todayDate,
+            "endHh": "01"
+        ]
+        
+        do {
+            let response = try await WeatherAPI.requestWeather(target: .fetchPastWeather(parameter: parameter))
+            XCTAssertEqual(response.statusCode, 200)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+}
