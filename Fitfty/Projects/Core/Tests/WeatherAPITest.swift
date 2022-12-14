11eada010daa5002bfbbd0476ff08e17582b3fc2
EthanPark <ethanpark604@gmail.com>
2022-12-15 01:57:38 +0900

    ✅ [test] WeatherAPITest 환경변수 고려하여 테스트 코드 작성

--- Fitfty/Projects/Core/Tests/WeatherAPITest.swift
+++ Fitfty/Projects/Core/Tests/WeatherAPITest.swift
@@ -9,6 +9,8 @@ import XCTest
 @testable import Core
 
 final class WeatherAPITests: XCTestCase {
+    var parameter: [String: Any] = [:]
+    var apiKey: String = ProcessInfo.processInfo.environment["WEATHER_API_KEY"]?.removingPercentEncoding ?? "NoAPIKey"
     var todayDate: String {
         let date = Date()
         let dateFormatter = DateFormatter()
@@ -17,8 +19,19 @@ final class WeatherAPITests: XCTestCase {
         return dateFormatter.string(from: date)
     }
     
-    func testFetchWeatherAPI_statusCode가_200인지() async throws {
-        let parameter: [String: Any] = [
+    override class func setUp() {}
+    
+    /// 로컬환경에서는 해당 테스트 Skip
+    func test_EnviornmentVariable가_파싱됐는지() throws {
+        let isSkippable = APIKey.apiKey.isEmpty ? false : true
+        
+        try XCTSkipIf(isSkippable)
+        XCTAssertFalse(apiKey == "NoAPIKey", "Fail To Parsing")
+    }
+    
+    func test_FetchWeatherAPI_statusCode가_200인지() async throws {
+        // Given
+        parameter = [
             "numOfRows": "10",
             "pageNo": "1",
             "base_date": todayDate,
@@ -26,17 +39,21 @@ final class WeatherAPITests: XCTestCase {
             "nx": "55",
             "ny": "127"
         ]
+        parameter.updateValue(apiKey, forKey: "serviceKey")
         
         do {
+            // When
             let response = try await WeatherAPI.requestWeather(target: .fetchWeather(parameter: parameter))
+            // Then
             XCTAssertEqual(response.statusCode, 200)
         } catch {
             XCTFail(error.localizedDescription)
         }
     }
     
-    func testPastFetchWeatherAPI_statusCode가_200인지() async throws {
-        let parameter: [String: Any] = [
+    func test_PastFetchWeatherAPI_statusCode가_200인지() async throws {
+        // Given
+        parameter = [
             "numOfRows": "10",
             "pageNo": "1",
             "dataCd": "ASOS",
@@ -47,9 +64,12 @@ final class WeatherAPITests: XCTestCase {
             "endDt": "20221207",
             "endHh": "01"
         ]
-        
+        parameter.updateValue(apiKey, forKey: "serviceKey")
+
         do {
+            // When
             let response = try await WeatherAPI.requestWeather(target: .fetchPastWeather(parameter: parameter))
+            // Then
             XCTAssertEqual(response.statusCode, 200)
         } catch {
             XCTFail(error.localizedDescription)
