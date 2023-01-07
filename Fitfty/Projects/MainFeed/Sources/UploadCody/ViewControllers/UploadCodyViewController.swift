69f12035725da0dfb3170a39e937f3a897212250
youngsun <dudtjs1021@hansung.ac.kr>
2023-01-08 01:31:56 +0900

    ✨ [feat] UploadCodyViewController의 ContentCell 구현
    - 인스타그램 버튼 이미지 추가
    - CollectionView를 사용해 ContentCell 구현

--- Fitfty/Projects/MainFeed/Sources/UploadCody/ViewControllers/UploadCodyViewController.swift
+++ Fitfty/Projects/MainFeed/Sources/UploadCody/ViewControllers/UploadCodyViewController.swift
@@ -10,14 +10,41 @@ import UIKit
 
 final public class UploadCodyViewController: UIViewController {
     
+    enum Section {
+        
+        case content
+        case weatherTag
+        case styleTag
+        
+        init?(index: Int) {
+            switch index {
+            case 0: self = .content
+            case 1: self = .weatherTag
+            case 2: self = .styleTag
+            default: return nil
+            }
+        }
+        
+    }
+    
     private var coordinator: UploadCodyCoordinatorInterface
+    private var dataSource: UICollectionViewDiffableDataSource<Section, UUID>?
     
     private let topBarView = TopBarView()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(ContentCell.self)
+        return collectionView
+    }()
 
     public override func viewDidLoad() {
         super.viewDidLoad()
         setUpConstraintLayout()
         setUpButtonAction()
+        setUpDataSource()
+        applySnapshot()
     }
 
     public init(coordinator: UploadCodyCoordinatorInterface) {
@@ -31,13 +58,87 @@ final public class UploadCodyViewController: UIViewController {
     }
     
     private func setUpConstraintLayout() {
-        view.addSubviews(topBarView)
+        view.addSubviews(topBarView, collectionView)
         NSLayoutConstraint.activate([
             topBarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             topBarView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
             topBarView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-            topBarView.heightAnchor.constraint(equalToConstant: 45)
+            topBarView.heightAnchor.constraint(equalToConstant: 50),
+            
+            collectionView.topAnchor.constraint(equalTo: topBarView.bottomAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+    
+    private func setUpDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, UUID>(
+            collectionView: collectionView,
+            cellProvider: { collectionView, indexPath, _ in
+                let section = Section(index: indexPath.section)
+                switch section {
+                case .content:
+                    let cell = collectionView.dequeueReusableCell(ContentCell.self, for: indexPath)
+                    return cell
+                    
+                case .weatherTag:
+                    let cell = collectionView.dequeueReusableCell(ContentCell.self, for: indexPath)
+                    return cell
+                    
+                case .styleTag:
+                    let cell = collectionView.dequeueReusableCell(ContentCell.self, for: indexPath)
+                    return cell
+                    
+                default:
+                    return UICollectionViewCell()
+                }
+            })
+       
+        collectionView.dataSource = dataSource
+    }
+    
+    private func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, UUID>()
+        snapshot.appendSections([.content])
+        snapshot.appendItems([UUID()])
+        snapshot.appendSections([.weatherTag])
+        snapshot.appendItems([UUID()])
+        snapshot.appendSections([.styleTag])
+        snapshot.appendItems([UUID()])
+        dataSource?.apply(snapshot)
+    }
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { [weak self] (sectionNumber, _) -> NSCollectionLayoutSection? in
+            let section = Section(index: sectionNumber)
+            switch section {
+            case .content: return self?.contentSectionLayout()
+            case .weatherTag: return self?.contentSectionLayout()
+            case .styleTag: return self?.contentSectionLayout()
+            default: return nil
+            }
+        }
+    }
+    
+    private func contentSectionLayout() -> NSCollectionLayoutSection? {
+        let layoutSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: .init(
+                widthDimension: layoutSize.widthDimension,
+                heightDimension: layoutSize.heightDimension
+            ),
+            subitems: [.init(layoutSize: layoutSize)]
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: .zero, leading: .zero, bottom: .zero, trailing: .zero)
+        section.orthogonalScrollingBehavior = .groupPaging
+        
+        return section
     }
     
     private func setUpButtonAction() {
