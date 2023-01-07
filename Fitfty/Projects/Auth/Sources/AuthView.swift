b9dcfae1237618378034e891a5f7d73830ead561
EthanPark <ethanpark604@gmail.com>
2023-01-07 20:59:03 +0900

    ✨ [feat] AuthView 작업

--- Fitfty/Projects/Auth/Sources/AuthView.swift
+++ Fitfty/Projects/Auth/Sources/AuthView.swift
@@ -16,7 +16,7 @@ final class AuthView: UIView {
     private let kakaoButton = UIButton()
     private let appleButton = UIButton()
     private let enterWithoutLoginButton = UIButton()
-    private let loginProblemLabel = UILabel()
+    private let loginProblemButton = UIButton()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,13 +27,25 @@ final class AuthView: UIView {
         super.init(coder: coder)
     }
     
-    func setTargetEnterButtonWithoutLogin() {
-        
+    func setEnterWithoutLoginButtonTarget(target: Any?, action: Selector) {
+        enterWithoutLoginButton.addTarget(target, action: action, for: .touchUpInside)
+    }
+    
+    func setKakaoButtonTarget(target: Any?, action: Selector) {
+        kakaoButton.addTarget(target, action: action, for: .touchUpInside)
+    }
+    
+    func setAppleButtonTarget(target: Any?, action: Selector) {
+        appleButton.addTarget(target, action: action, for: .touchUpInside)
+    }
+    
+    func setLoginProblemButtonTarget(target: Any?, action: Selector) {
+        loginProblemButton.addTarget(target, action: action, for: .touchUpInside)
     }
     
     private func configure() {
         configureLogoImageView()
-        configureLoginProblemLabel()
+        configureLoginProblemButton()
         configureEnterButtonWithoutLogin()
         configureSnsImageStackView()
         configureSnsLabel()
@@ -43,7 +55,7 @@ final class AuthView: UIView {
         addSubviews(logoImageView)
         NSLayoutConstraint.activate([
             logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            logoImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            logoImageView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -Style.LogoImageView.margin),
             logoImageView.widthAnchor.constraint(equalToConstant: Style.LogoImageView.size.width),
             logoImageView.heightAnchor.constraint(equalToConstant: Style.LogoImageView.size.height)
         ])
@@ -51,32 +63,35 @@ final class AuthView: UIView {
         logoImageView.image = Style.LogoImageView.image
     }
     
-    private func configureLoginProblemLabel() {
-        addSubviews(loginProblemLabel)
+    private func configureLoginProblemButton() {
+        addSubviews(loginProblemButton)
         NSLayoutConstraint.activate([
-            loginProblemLabel.bottomAnchor.constraint(equalTo: bottomAnchor,
+            loginProblemButton.bottomAnchor.constraint(equalTo: bottomAnchor,
                                                       constant: -Style.LoginProblemLabel.margin),
-            loginProblemLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+            loginProblemButton.centerXAnchor.constraint(equalTo: centerXAnchor)
         ])
         
-        loginProblemLabel.text = Style.LoginProblemLabel.text
-        loginProblemLabel.font = Style.LoginProblemLabel.font
+        loginProblemButton.titleLabel?.font = Style.LoginProblemLabel.font
+        loginProblemButton.setTitle(Style.LoginProblemLabel.text, for: .normal)
+        loginProblemButton.setTitleColor(Style.LoginProblemLabel.textColor.color, for: .normal)
     }
     
     private func configureEnterButtonWithoutLogin() {
         addSubviews(enterWithoutLoginButton)
         NSLayoutConstraint.activate([
-            enterWithoutLoginButton.bottomAnchor.constraint(equalTo: loginProblemLabel.topAnchor,
+            enterWithoutLoginButton.bottomAnchor.constraint(equalTo: loginProblemButton.topAnchor,
                                                             constant: -Style.EnterWithoutLoginButton.margin),
             enterWithoutLoginButton.centerXAnchor.constraint(equalTo: centerXAnchor),
             enterWithoutLoginButton.widthAnchor.constraint(equalToConstant: Style.EnterWithoutLoginButton.size.width),
             enterWithoutLoginButton.heightAnchor.constraint(equalToConstant: Style.EnterWithoutLoginButton.size.height)
         ])
         
+        enterWithoutLoginButton.titleLabel?.font = Style.EnterWithoutLoginButton.font
         enterWithoutLoginButton.setTitle(Style.EnterWithoutLoginButton.title, for: .normal)
         enterWithoutLoginButton.setTitleColor(Style.EnterWithoutLoginButton.textColor.color, for: .normal)
         enterWithoutLoginButton.layer.borderColor = Style.EnterWithoutLoginButton.borderColor.color.cgColor
         enterWithoutLoginButton.layer.cornerRadius = Style.EnterWithoutLoginButton.radius
+        enterWithoutLoginButton.layer.borderWidth = Style.EnterWithoutLoginButton.borderWidth
     }
     
     private func configureSnsImageStackView() {
@@ -118,10 +133,14 @@ final class AuthView: UIView {
     private func configureSnsLabel() {
         addSubviews(snsLabel)
         NSLayoutConstraint.activate([
-            snsImageStackView.bottomAnchor.constraint(equalTo: snsImageStackView.topAnchor,
+            snsLabel.bottomAnchor.constraint(equalTo: snsImageStackView.topAnchor,
                                                       constant: -Style.SnsLabel.margin),
-            snsImageStackView.centerXAnchor.constraint(equalTo: centerXAnchor)
+            snsLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
         ])
+        
+        snsLabel.text = Style.SnsLabel.text
+        snsLabel.font = Style.SnsLabel.font
+        snsLabel.textColor = Style.SnsLabel.textColor.color
     }
 }
 
@@ -130,21 +149,23 @@ private extension AuthView {
         enum LogoImageView {
             static let size = CGSize(width: 103, height: 78)
             static let image = CommonAsset.Images.fitftyLogoImage.image
+            static let margin: CGFloat = 80
         }
         
         enum LoginProblemLabel {
             static let margin: CGFloat = 72
             static let text = "로그인에 문제가 있나요?"
-            static let textColor = CommonAsset.Colors.gray2Light
+            static let textColor = CommonAsset.Colors.gray04
             static let font = FitftyFont.appleSDMedium(size: 13).font
         }
         
         enum EnterWithoutLoginButton {
             static let size = CGSize(width: 166, height: 38)
             static let margin: CGFloat = 20
-            static let borderColor = CommonAsset.Colors.gray5Light
-            static let textColor = CommonAsset.Colors.gray5Light
-            static let radius: CGFloat = 32
+            static let borderColor = CommonAsset.Colors.gray02
+            static let borderWidth: CGFloat = 1
+            static let textColor = CommonAsset.Colors.gray05
+            static let radius: CGFloat = 16
             static let font = FitftyFont.appleSDMedium(size: 13).font
             static let title = "둘러보기"
         }
@@ -169,7 +190,7 @@ private extension AuthView {
         enum SnsLabel {
             static let margin: CGFloat = 16
             static let text = "SNS 계정으로 간편 가입하기"
-            static let textColor = CommonAsset.Colors.grayLight
+            static let textColor = CommonAsset.Colors.gray05
             static let font = FitftyFont.appleSDMedium(size: 13).font
         }
     }
