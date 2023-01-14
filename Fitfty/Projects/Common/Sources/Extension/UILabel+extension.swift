65d1d1a6b78a06ddb38966ee38445cf97554bb58
Ari <lee_ari95@icloud.com>
2023-01-14 16:47:43 +0900

    ✨ [feat] 주소 목록을 나타내는 AddressCell을 구현

---
+++ Fitfty/Projects/Common/Sources/Extension/UILabel+extension.swift
@@ -0,0 +1,23 @@
+//
+//  UILabel+extension.swift
+//  Common
+//
+//  Created by Ari on 2023/01/08.
+//  Copyright © 2023 Fitfty. All rights reserved.
+//
+
+import UIKit
+
+public extension UILabel {
+    
+    func editTextColor(of prefix: String, in color: UIColor) {
+        guard let text = text else {
+            return
+        }
+        let attributedString = NSMutableAttributedString(string: text)
+        let range = (text as NSString).range(of: prefix)
+        attributedString.addAttribute(.foregroundColor, value: color, range: range)
+        attributedText = attributedString
+    }
+    
+}
