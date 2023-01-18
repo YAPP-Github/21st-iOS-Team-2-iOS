1cb0fdbbd84619e5dfcd09e505b9dd53c2853b97
Ari <lee_ari95@icloud.com>
2023-01-18 21:35:19 +0900

    ✨ [feat] WeatherAPI 내부 리팩토링 및 WeatherRepository 에러 처리 관련 로직 개선
    - 기존에 디코딩을 시도할 때 옵셔널한 try로 시도하던 부분을 개선함
    - 에러 로그를 남겨주는 Logger를 구현

---
+++ Fitfty/Projects/Core/Sources/Logger.swift
@@ -0,0 +1,25 @@
+//
+//  Logger.swift
+//  Core
+//
+//  Created by Ari on 2023/01/18.
+//  Copyright © 2023 Fitfty. All rights reserved.
+//
+
+import Foundation
+
+public enum Logger {
+    
+    public static func debug(
+        error: Error,
+        message: String,
+        file: String = #file,
+        function: String = #function,
+        line: Int = #line
+    ) {
+        let file = file.components(separatedBy: ["/"]).last ?? ""
+        print("\(Date().toString(.log)) [⛔️][\(file)][\(function)][\(line)] -> \(message)")
+        debugPrint(error)
+    }
+    
+}
