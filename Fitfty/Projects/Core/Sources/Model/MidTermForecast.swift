9b0a1252dce8940ef500a478be69302f750cf439
Ari <lee_ari95@icloud.com>
2023-01-23 10:03:36 +0900

    ✨ [feat] 중기예보 모델 및 요청 타입 개선
    - 중기예보 요청시 필요한 지역코드를 가지는 타입 구현

---
+++ Fitfty/Projects/Core/Sources/Model/MidTermForecast.swift
@@ -0,0 +1,30 @@
+//
+//  MidTermForecast.swift
+//  Core
+//
+//  Created by Ari on 2023/01/21.
+//  Copyright © 2023 Fitfty. All rights reserved.
+//
+
+import Foundation
+
+public struct MidTermForecast {
+    
+    public let meridiem: Meridiem
+    public let date: Date
+    public let forecast: Forecast
+    public let precipitation: Int
+    public let maxTemp: Int
+    public let minTemp: Int
+    
+}
+
+public enum Forecast: String {
+    case sunny = "맑음"
+    case lostOfCloudy = "구름많음"
+    case cloudy = "흐림"
+    case rain = "비"
+    case rainOrSnow = "비 또는 눈"
+    case snow = "눈"
+    case scurry = "소나기"
+}
