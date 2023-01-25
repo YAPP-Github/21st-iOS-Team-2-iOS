97d87c3a956df198677becfbadb4759dddd47d89
Ethan <>
2023-01-25 22:52:14 +0900

    ✨ [feat] Intro, PermissionView 작업

--- Fitfty/Projects/Auth/Sources/IntroViewController.swift
+++ Fitfty/Projects/Auth/Sources/IntroViewController.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2023 Fitfty. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final public class IntroViewController: UIViewController {
+    private let coordinator: IntroCoordinatorInterface
+    
+    private let titleLabel = UILabel()
+    private let introImageView = UIImageView()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public init(coordinator: IntroCoordinatorInterface) {
+        self.coordinator = coordinator
+        super.init(nibName: nil, bundle: nil)
+    }
+}
