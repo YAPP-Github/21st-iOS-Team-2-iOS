dac5acde3ef08de2e5df98c50e8be89c8154caa2
Ari <lee_ari95@icloud.com>
2023-01-28 19:27:04 +0900

    ✨ [feat] 설정 화면에 쓰일 Setting 모델을 구현

---
+++ Fitfty/Projects/Profile/Sources/Setting/Setting.swift
@@ -0,0 +1,48 @@
+//
+//  Setting.swift
+//  Profile
+//
+//  Created by Ari on 2023/01/28.
+//  Copyright © 2023 Fitfty. All rights reserved.
+//
+
+import Foundation
+
+enum Setting {
+    case profile
+    case myInfo
+    case feed
+    case pushNoti
+    case logout
+    case membershipWithdrawal
+}
+
+extension Setting{
+    
+    var title: String {
+        switch self {
+        case .profile: return "프로필 설정"
+        case .myInfo: return "내 정보 설정"
+        case .feed: return "핏프티 피드 정보 설정"
+        case .pushNoti: return "푸쉬 알림 설정"
+        case .logout: return "로그아웃"
+        case .membershipWithdrawal: return "회원 탈퇴"
+        }
+    }
+    
+    var isNextPage: Bool {
+        switch self {
+        case .profile, .myInfo, .feed, .pushNoti: return true
+        case .logout, .membershipWithdrawal: return false
+        }
+    }
+    
+    static func settings() -> [Setting] {
+        return [.profile, .myInfo, .feed, .pushNoti]
+    }
+    
+    static func etc() -> [Setting] {
+        return [.logout, .membershipWithdrawal]
+    }
+    
+}
