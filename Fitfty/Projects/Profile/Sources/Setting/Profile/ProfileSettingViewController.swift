3ba8d845db912fd3d938ab30769e7107dee9b0ce
Ari <lee_ari95@icloud.com>
2023-01-28 21:29:55 +0900

    ✨ [feat] 프로필 설정 화면 UI 구현

--- Fitfty/Projects/Profile/Sources/Setting/Profile/ProfileSettingViewController.swift
+++ Fitfty/Projects/Profile/Sources/Setting/Profile/ProfileSettingViewController.swift
@@ -34,6 +34,53 @@ public final class ProfileSettingViewController: UIViewController {
         barView.setCancelButtonTarget(target: self, action: #selector(didTapCancelButton(_:)))
         return barView
     }()
+    
+    private lazy var profileView: UIStackView = {
+        let stackView = UIStackView(axis: .vertical, alignment: .center, distribution: .fill, spacing: 12)
+        stackView.addArrangedSubviews(profileImageView, editProfileButton)
+        return stackView
+    }()
+    
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = CommonAsset.Images.profileSample.image
+        imageView.widthAnchor.constraint(equalToConstant: 104).isActive = true
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 104 / 2
+        return imageView
+    }()
+    
+    private lazy var editProfileButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("프로필 사진 변경", for: .normal)
+        button.setTitleColor(CommonAsset.Colors.gray05.color, for: .normal)
+        button.titleLabel?.font = FitftyFont.appleSDSemiBold(size: 13).font
+        button.layer.borderWidth = 1
+        button.layer.borderColor = CommonAsset.Colors.gray01.color.cgColor
+        button.layer.cornerRadius = 16
+        button.widthAnchor.constraint(equalToConstant: 121).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 34).isActive = true
+        button.addTarget(self, action: #selector(didTapEditProfileButton(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var introductionView: UserInputView = {
+        let inputView = UserInputView(title: "한 줄 소개", textField: introductionTextField)
+        return inputView
+    }()
+    
+    private lazy var introductionTextField: FitftyTextField = {
+        let textFiled = FitftyTextField(style: .normal, placeHolderText: "20자 이내 한 줄 소개를 입력해주세요.")
+        textFiled.becomeFirstResponder()
+        return textFiled
+    }()
+    
+    private lazy var cancelButton: FitftyButton = {
+        let button = FitftyButton(style: .enabled, title: "닫기")
+        button.setButtonTarget(target: self, action: #selector(didTapCancelButton(_:)))
+        return button
+    }()
 }
 
 private extension ProfileSettingViewController {
@@ -44,17 +91,29 @@ private extension ProfileSettingViewController {
     
     func setUpLayout() {
         view.backgroundColor = .white
-        view.addSubviews(navigationBarView)
+        view.addSubviews(navigationBarView, profileView, introductionView, cancelButton)
         NSLayoutConstraint.activate([
             navigationBarView.topAnchor.constraint(equalTo: view.topAnchor),
             navigationBarView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             navigationBarView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            navigationBarView.heightAnchor.constraint(equalToConstant: 76)
+            navigationBarView.heightAnchor.constraint(equalToConstant: 76),
+            profileView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileView.topAnchor.constraint(equalTo: navigationBarView.bottomAnchor),
+            introductionView.topAnchor.constraint(equalTo: profileView.bottomAnchor, constant: 24),
+            introductionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            introductionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            cancelButton.topAnchor.constraint(equalTo: introductionView.bottomAnchor, constant: 32),
+            cancelButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            cancelButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
         ])
     }
     
     @objc func didTapCancelButton(_ sender: UITapGestureRecognizer) {
         coordinator?.dismiss()
+    }
+    
+    @objc func didTapEditProfileButton(_ sender: UIButton) {
+        print(#function)
     }
     
 }
