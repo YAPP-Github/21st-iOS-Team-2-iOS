4e41a4116cd5c5234eb83bd8305fdc3bfb5bdef5
Ari <lee_ari95@icloud.com>
2023-01-29 16:32:34 +0900

    💄 [style] 코디를 보여주는 셀 레이아웃 및 뷰 스타일을 수정
    - 북마크 아이콘, 조회수 아이콘 추가

--- Fitfty/Projects/MainFeed/Sources/Main/Views/Main/CodyCell.swift
+++ Fitfty/Projects/MainFeed/Sources/Main/Views/Main/CodyCell.swift
@@ -11,65 +11,11 @@ import Common
 
 final class CodyCell: UICollectionViewCell {
     
+    private var isBookmark: Bool = false
+    
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: 300, height: 336)
+        return CGSize(width: 256, height: 256)
     }
-    
-    private lazy var codyImageView: UIImageView = {
-        let image = UIImageView()
-        image.layer.cornerRadius = 15
-        image.clipsToBounds = true
-        image.image = CommonAsset.Images.sample.image
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = CGRect(x: .zero, y: .zero, width: 300, height: 336)
-        gradientLayer.colors = [UIColor.white.withAlphaComponent(0).cgColor, CommonAsset.Colors.ft.color.cgColor]
-        gradientLayer.locations = [0.35, 0.8]
-        gradientLayer.cornerRadius = 15
-        image.layer.addSublayer(gradientLayer)
-        return image
-    }()
-    
-    private lazy var profileStackView: UIStackView = {
-        let stackView = UIStackView(axis: .horizontal, alignment: .center, distribution: .fill, spacing: 8)
-        stackView.backgroundColor = .clear
-        stackView.addArrangedSubviews(profileImageView, profileInfoStackView)
-        return stackView
-    }()
-    
-    private lazy var profileImageView: UIImageView = {
-        let image = UIImageView()
-        image.image = CommonAsset.Images.sample.image
-        image.clipsToBounds = true
-        image.layer.cornerRadius = 20
-        image.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        image.heightAnchor.constraint(equalTo: image.widthAnchor).isActive = true
-        return image
-    }()
-    
-    private lazy var profileInfoStackView: UIStackView = {
-        let stackView = UIStackView(axis: .vertical, alignment: .leading, distribution: .fill, spacing: 2)
-        stackView.backgroundColor = .clear
-        stackView.addArrangedSubviews(nameLabel, dateLabel)
-        return stackView
-    }()
-    
-    private lazy var nameLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .preferredFont(for: .footnote, weight: .bold)
-        label.setContentHuggingPriority(.required, for: .horizontal)
-        label.text = "iloveiso2"
-        return label
-    }()
-    
-    private lazy var dateLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = CommonAsset.Colors.ftSecondaryText.color
-        label.font = .preferredFont(for: .footnote, weight: .semibold)
-        label.setContentHuggingPriority(.required, for: .horizontal)
-        label.text = "4시간"
-        return label
-    }()
     
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -93,26 +39,82 @@ final class CodyCell: UICollectionViewCell {
         return CGSize(width: intrinsicContentSize.width, height: intrinsicContentSize.height)
     }
     
-    private func configure() {
-        contentView.addSubviews(codyImageView, profileStackView)
-        NSLayoutConstraint.activate([
-            codyImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            codyImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            codyImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            codyImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            profileStackView.leadingAnchor.constraint(equalTo: codyImageView.leadingAnchor, constant: 20),
-            profileStackView.bottomAnchor.constraint(equalTo: codyImageView.bottomAnchor, constant: -20)
-        ])
-    }
-    
-    private func reset() {
-        codyImageView.image = nil
-    }
-    
     func addProfileViewGestureRecognizer(_ target: Any?, action: Selector) {
         profileStackView.addGestureRecognizer(UITapGestureRecognizer(target: target, action: action))
     }
     
+    private lazy var viewsInfoView: UIStackView = {
+        let stackView = UIStackView(axis: .horizontal, alignment: .leading, distribution: .fill, spacing: 4)
+        stackView.addArrangedSubviews(viewsIconImageView, viewsCountLabel)
+        return stackView
+    }()
+    
+    private lazy var viewsIconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = CommonAsset.Images.eye.image
+        imageView.widthAnchor.constraint(equalToConstant: 16).isActive = true
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+        return imageView
+    }()
+    
+    private lazy var viewsCountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = FitftyFont.SFProDisplayBold(size: 12).font
+        label.text = "1245".insertComma
+        return label
+    }()
+    
+    private lazy var codyImageView: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = 15
+        image.clipsToBounds = true
+        image.image = CommonAsset.Images.sample.image
+        let dimmedView = UIView()
+        dimmedView.backgroundColor = .black.withAlphaComponent(0.4)
+        image.addSubviews(dimmedView)
+        dimmedView.widthAnchor.constraint(equalToConstant: 256).isActive = true
+        dimmedView.heightAnchor.constraint(equalToConstant: 61).isActive = true
+        dimmedView.bottomAnchor.constraint(equalTo: image.bottomAnchor).isActive = true
+        dimmedView.centerXAnchor.constraint(equalTo: image.centerXAnchor).isActive = true
+        return image
+    }()
+    
+    private lazy var profileStackView: UIStackView = {
+        let stackView = UIStackView(axis: .horizontal, alignment: .center, distribution: .fill, spacing: 8)
+        stackView.backgroundColor = .clear
+        stackView.addArrangedSubviews(profileImageView, nameLabel)
+        return stackView
+    }()
+    
+    private lazy var profileImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = CommonAsset.Images.sample.image
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 16
+        image.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        image.heightAnchor.constraint(equalTo: image.widthAnchor).isActive = true
+        return image
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = FitftyFont.SFProDisplaySemibold(size: 13).font
+        label.setContentHuggingPriority(.required, for: .horizontal)
+        label.text = "iloveiso2"
+        return label
+    }()
+    
+    private lazy var bookmarkButton: UIButton = {
+        let button = UIButton()
+        button.setImage(CommonAsset.Images.bookmark.image, for: .normal)
+        button.tintColor = .white
+        button.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        button.heightAnchor.constraint(equalTo: button.widthAnchor).isActive = true
+        button.addTarget(self, action: #selector(didTapBookmarkButton(_:)), for: .touchUpInside)
+        return button
+    }()
 }
 
 extension CodyCell {
@@ -121,4 +123,36 @@ extension CodyCell {
         
     }
     
+}
+
+private extension CodyCell {
+    
+    func reset() {
+        codyImageView.image = nil
+        viewsCountLabel.text = nil
+        nameLabel.text = nil
+    }
+    
+    func configure() {
+        contentView.addSubviews(codyImageView, profileStackView, viewsInfoView, bookmarkButton)
+        NSLayoutConstraint.activate([
+            codyImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            codyImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            codyImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            codyImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            profileStackView.leadingAnchor.constraint(equalTo: codyImageView.leadingAnchor, constant: 12),
+            profileStackView.bottomAnchor.constraint(equalTo: codyImageView.bottomAnchor, constant: -15),
+            viewsInfoView.topAnchor.constraint(equalTo: codyImageView.topAnchor, constant: 12),
+            viewsInfoView.leadingAnchor.constraint(equalTo: codyImageView.leadingAnchor, constant: 12),
+            bookmarkButton.bottomAnchor.constraint(equalTo: codyImageView.bottomAnchor, constant: -8),
+            bookmarkButton.trailingAnchor.constraint(equalTo: codyImageView.trailingAnchor, constant: -8)
+        ])
+    }
+    
+    @objc func didTapBookmarkButton(_ sender: UIButton) {
+        isBookmark = !isBookmark
+        bookmarkButton.setImage(
+            isBookmark ? CommonAsset.Images.bookmarkFill.image : CommonAsset.Images.bookmark.image, for: .normal
+        )
+    }
 }
