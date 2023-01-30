167386f579d254e7907920e2a23ab1b38ed8be3b
youngsun <dudtjs1021@hansung.ac.kr>
2023-01-30 19:22:40 +0900

    ✨ [feat] ProfileType 추가

---
+++ Fitfty/Projects/Profile/Sources/ProfileType.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileType.swift
+//  Profile
+//
+//  Created by 임영선 on 2023/01/30.
+//  Copyright © 2023 Fitfty. All rights reserved.
+//
+
+import Foundation
+
+public enum ProfileType {
+    case myProfile
+    case userProfile
+}
