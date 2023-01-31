2c10cec907be6f8ab86fa20cd822e704695ddb17
youngsun <dudtjs1021@hansung.ac.kr>
2023-01-31 10:29:53 +0900

    ✨ [feat] Profile PresentType 추기

---
+++ Fitfty/Projects/Profile/Sources/PresentType.swift
@@ -0,0 +1,14 @@
+//
+//  PresentType.swift
+//  Profile
+//
+//  Created by 임영선 on 2023/01/31.
+//  Copyright © 2023 Fitfty. All rights reserved.
+//
+
+import Foundation
+
+public enum PresentType {
+    case main
+    case tab
+}
