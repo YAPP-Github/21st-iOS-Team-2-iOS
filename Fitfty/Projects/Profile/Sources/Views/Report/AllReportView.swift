9b94cd332e6bb0c1426065557b1e9bea7b567400
youngsun <dudtjs1021@hansung.ac.kr>
2023-01-31 17:50:10 +0900

    ✨ [feat] DetailReportViewController 구현
    - ReportView, AllReportView 추가

---
+++ Fitfty/Projects/Profile/Sources/Views/Report/AllReportView.swift
@@ -0,0 +1,40 @@
+//
+//  AllReportView.swift
+//  Profile
+//
+//  Created by 임영선 on 2023/01/31.
+//  Copyright © 2023 Fitfty. All rights reserved.
+//
+
+import UIKit
+import Common
+
+final class AllReportView: UIStackView {
+    
+    private let reportViews: [ReportView] = [
+        ReportView(title: "음란성 / 선정성 게시물"),
+        ReportView(title: "날씨 태그가 사진과 어울리지 않음"),
+        ReportView(title: "지적재산권 침해"),
+        ReportView(title: "혐오 / 욕설 / 인신공격"),
+        ReportView(title: "같은 내용 반복 게시"),
+        ReportView(title: "기타 (team.fitfty@gmail.com로 제보)")
+    ]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setStackView()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setStackView() {
+        addArrangedSubviews(reportViews.map { $0 })
+        self.distribution = .fill
+        self.alignment = .fill
+        self.spacing = 23
+    }
+    
+}
+
---
+++ Fitfty/Projects/Profile/Sources/Views/Report/ReportView.swift
@@ -0,0 +1,51 @@
+//
+//  ReportView.swift
+//  Profile
+//
+//  Created by 임영선 on 2023/01/31.
+//  Copyright © 2023 Fitfty. All rights reserved.
+//
+
+import UIKit
+import Common
+
+final class ReportView: UIStackView {
+    
+    private lazy var checkBoxButton: UIButton = {
+        let button = UIButton()
+        button.setImage(CommonAsset.Images.btnCheckBoxUnSelected.image, for: .normal)
+        button.setImage(CommonAsset.Images.btnCheckBoxSelected.image, for: .selected)
+        return button
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = CommonAsset.Colors.gray06.color
+        label.font = FitftyFont.appleSDMedium(size: 15).font
+        return label
+    }()
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        titleLabel.text = title
+        setStackView()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setStackView() {
+        addArrangedSubviews(checkBoxButton, titleLabel)
+        self.axis = .horizontal
+        self.alignment = .leading
+        self.distribution = .fill
+        self.spacing = 12
+    }
+}
+
+extension ReportView {
+    func setButton(isSelected: Bool) {
+        checkBoxButton.isSelected = isSelected
+    }
+}
