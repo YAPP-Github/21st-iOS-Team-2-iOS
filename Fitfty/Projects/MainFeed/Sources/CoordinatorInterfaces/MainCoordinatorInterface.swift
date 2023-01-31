bc59ade03690530d5013e8ddd405acfeeb515090
Ari <lee_ari95@icloud.com>
2023-01-31 22:47:02 +0900

    ♻️ [refactor] import Foundation 추가

--- Fitfty/Projects/MainFeed/Sources/CoordinatorInterfaces/MainCoordinatorInterface.swift
+++ Fitfty/Projects/MainFeed/Sources/CoordinatorInterfaces/MainCoordinatorInterface.swift
@@ -5,6 +5,8 @@
 //  Created by Ari on 2022/12/05.
 //  Copyright © 2022 Fitfty. All rights reserved.
 //
+
+import Foundation
 import Profile
 
 public protocol MainCoordinatorInterface: AnyObject {
