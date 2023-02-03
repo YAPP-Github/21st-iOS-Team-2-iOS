88a27c523f4b314f129001441047e6804ff4044d
Ari <lee_ari95@icloud.com>
2023-02-03 23:21:28 +0900

    ✨ [feat] MainViewModel 내부에 Input, Output을 구현
    - 현재 위치를 활용하여 주소를 가져오는 기능 추가

--- Fitfty/Projects/MainFeed/Sources/Main/Home/ViewModel/MainViewModel.swift
+++ Fitfty/Projects/MainFeed/Sources/Main/Home/ViewModel/MainViewModel.swift
@@ -9,6 +9,8 @@
 import Foundation
 import Combine
 import Common
+import Core
+import CoreLocation
 
 enum MainViewSection {
     case weather
@@ -25,16 +27,91 @@ enum MainViewSection {
     }
 }
 
+protocol MainViewModelInput {
+    
+    var input: MainViewModelInput { get }
+    
+    func viewDidLoad()
+    
+    func viewWillAppear()
+    
+    func viewDidAppear()
+}
+
 public final class MainViewModel {
-
+    
     private var currentState: CurrentValueSubject<ViewModelState?, Never> = .init(nil)
+    private var cancellables: Set<AnyCancellable> = .init()
+    
+    private let addressRepository: AddressRepository
+    private let userManager: UserManager
+    
+    private var _location: CurrentValueSubject<CLLocation?, Never> = .init(nil)
 
-    public init() {}
+    public init(
+        addressRepository: AddressRepository = DefaultAddressRepository(),
+        userManager: UserManager = DefaultUserManager()
+    ) {
+        self.addressRepository = addressRepository
+        self.userManager = userManager
+    }
 
 }
 
 extension MainViewModel: ViewModelType {
-    public enum ViewModelState {}
+    public enum ViewModelState {
+        case currentLocation(Address)
+        case errorMessage(String)
+    }
     
     public var state: AnyPublisher<ViewModelState, Never> { currentState.compactMap { $0 }.eraseToAnyPublisher() }
+}
+
+extension MainViewModel: MainViewModelInput {
+    
+    var input: MainViewModelInput { self }
+    
+    func viewDidLoad() {
+        LocationManager.shared.currentLocation()
+            .sink(receiveValue: { [weak self] location in
+                Task { [weak self] in
+                    guard let self = self else {
+                        return
+                    }
+                    do {
+                        let address = try await self.getAddress(location: location)
+                        self.currentState.send(.currentLocation(address))
+                        self.userManager.updateCurrentLocation(address)
+                    } catch {
+                        Logger.debug(error: error, message: "사용자 위치 가져오기 실패")
+                        self.currentState.send(.errorMessage("현재 위치를 가져오는데 알 수 없는 에러가 발생했습니다."))
+                    }
+                }
+            }).store(in: &cancellables)
+    }
+    
+    func viewWillAppear() {
+        print(#function)
+    }
+    
+    func viewDidAppear() {
+        print(#function)
+    }
+    
+}
+
+private extension MainViewModel {
+    
+    func getAddress(location: CLLocation) async throws -> Address {
+        if let address = userManager.currentLocation {
+            return address
+        } else {
+            let address = try await self.addressRepository.fetchAddress(
+                longitude: location.coordinate.longitude,
+                latitude: location.coordinate.latitude
+            )
+            userManager.updateCurrentLocation(address)
+            return address
+        }
+    }
 }
