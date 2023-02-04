e09b8e65cc438fbf77753af2b3d921b520f244a4
Ethan <>
2023-02-04 21:44:23 +0900

    ✨ [feat] SocialLoginManager 작업

--- Fitfty/Projects/Core/Sources/SocialLoginManager.swift
+++ Fitfty/Projects/Core/Sources/SocialLoginManager.swift
@@ -8,13 +8,14 @@
 
 import Foundation
 import KakaoSDKUser
+import AuthenticationServices
 
 public enum SocialLoginType {
     case apple
     case kakao
 }
 
-final public class SocialLoginManager {
+final public class SocialLoginManager: NSObject {
     static public let shared = SocialLoginManager()
     
     var accessToken: String?
@@ -45,7 +46,35 @@ final public class SocialLoginManager {
                 self?.accessToken = oauthToken?.accessToken
                 self?.refreshToken = oauthToken?.refreshToken
                 
+                
                 completionHandler()
+            }
+        }
+    }
+    
+    private func getUserInfo(completionHandler: @escaping () -> Void,
+                             failedHandler: @escaping (Error) -> Void) {
+       
+        UserApi.shared.me() { (user, error) in
+            if let error = error {
+                print(error)
+            } else {
+                if let user = user {
+                    var scopes = [String]()
+                    if (user.kakaoAccount?.profileNeedsAgreement == true) { scopes.append("profile") }
+                    if (user.kakaoAccount?.emailNeedsAgreement == true) { scopes.append("account_email") }
+                            
+                    UserApi.shared.loginWithKakaoAccount(scopes: scopes) {[weak self] (oauthToken, error) in
+                        if let error = error {
+                            failedHandler(error)
+                        } else {
+                            self?.accessToken = oauthToken?.accessToken
+                            self?.refreshToken = oauthToken?.refreshToken
+                            
+                            completionHandler()
+                        }
+                    }
+                }
             }
         }
     }
@@ -53,9 +82,54 @@ final public class SocialLoginManager {
     private func tryAppleLogin(completionHandler: @escaping () -> Void,
                                failedHandler: @escaping (Error) -> Void) {
         
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+        
+        
     }
     
     private func isKakaoLoginAvailable() -> Bool {
         return UserApi.isKakaoTalkLoginAvailable()
+    }
+}
+
+extension SocialLoginManager: ASAuthorizationControllerDelegate {
+    public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        
+        switch authorization.credential {
+            // Apple ID
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            
+            // 계정 정보 가져오기
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            let idToken = appleIDCredential.identityToken!
+            let tokeStr = String(data: idToken, encoding: .utf8)
+         
+            print("userIdentifier: \(userIdentifier)")
+            print("userEmail: \(email ?? "")")
+            print("userName: \((fullName?.givenName ?? "") + (fullName?.familyName ?? ""))")
+            print("identityToken: \(String(describing: tokeStr))")
+            
+        default:
+            break
+        }
+    }
+    
+    public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        
+    }
+}
+
+extension SocialLoginManager: ASAuthorizationControllerPresentationContextProviding {
+    public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return (UIApplication.shared.keyWindow?.rootViewController?.view.window)!
     }
 }
