67664353218e3f8386f6562fc7c9df78076b4328
youngsun <dudtjs1021@hansung.ac.kr>
2023-02-05 16:37:09 +0900

    ✨ [feat] 앨범 리스트 UI 구현

--- Fitfty/Projects/MainFeed/Sources/MyFitfty/ViewControllers/AlbumListViewController.swift
+++ Fitfty/Projects/MainFeed/Sources/MyFitfty/ViewControllers/AlbumListViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 import Common
 
 final public class AlbumListViewController: UIViewController {
+    
+    enum Section: CaseIterable {
+        case main
+    }
 
     private let coordinator: AlbumListCoordinatorInterface
     
@@ -19,9 +23,20 @@ final public class AlbumListViewController: UIViewController {
         return barView
     }()
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.showsVerticalScrollIndicator = false
+        tableView.register(AlbumListCell.self, forCellReuseIdentifier: AlbumListCell.className)
+        tableView.delegate = self
+        tableView.separatorInset = .init(top: .zero, left: 20, bottom: .zero, right: 20)
+        return tableView
+    }()
+    
+    private var dataSource: UITableViewDiffableDataSource<Section, UUID>?
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-        setcontstratinsLayout()
+        setUp()
     }
     
     public init(coordinator: AlbumListCoordinatorInterface) {
@@ -35,12 +50,16 @@ final public class AlbumListViewController: UIViewController {
     }
     
     private func setcontstratinsLayout() {
-        view.addSubviews(navigationBarView)
+        view.addSubviews(navigationBarView, tableView)
         NSLayoutConstraint.activate([
             navigationBarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             navigationBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navigationBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            navigationBarView.heightAnchor.constraint(equalToConstant: 66)
+            navigationBarView.heightAnchor.constraint(equalToConstant: 66),
+            tableView.topAnchor.constraint(equalTo: navigationBarView.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
     
@@ -48,4 +67,39 @@ final public class AlbumListViewController: UIViewController {
         coordinator.dismiss()
     }
     
+    private func setUp() {
+        setcontstratinsLayout()
+        setDataSource()
+        applySnapshot()
+    }
+}
+
+private extension AlbumListViewController {
+    
+    func setDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, UUID>(
+            tableView: tableView, cellProvider: { tableView, indexPath, _ in
+                let cell = tableView.dequeueReusableCell(withIdentifier: AlbumListCell.className, for: indexPath) as? AlbumListCell
+                cell?.setUp(image: CommonAsset.Images.sample.image, title: "내 앨범", photoCount: "1236")
+                return cell
+            })
+    }
+    
+    func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, UUID>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0..<10).map { _ in UUID() })
+        dataSource?.apply(snapshot)
+    }
+}
+
+extension AlbumListViewController: UITableViewDelegate {
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        coordinator.dismiss()
+    }
 }
