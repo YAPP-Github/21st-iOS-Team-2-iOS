657fc3c6e7b2f517e28f3af44dc24c3c81f54dd6
Ari <lee_ari95@icloud.com>
2023-02-08 15:15:16 +0900

    ♻️ [refactor] 기존 스타일 태그를 기획에 맞춰 개선

--- Fitfty/Projects/Common/Sources/StyleTag.swift
+++ Fitfty/Projects/Common/Sources/StyleTag.swift
@@ -6,31 +6,25 @@
 //  Copyright © 2023 Fitfty. All rights reserved.
 //
 
-public enum StyleTag {
-    case minimal
-    case modern
-    case casual
-    case street
-    case lovely
-    case hip
-    case luxury
+
+public enum StyleTag: String, Codable {
+    case minimal = "MINIMAL"
+    case modern = "MODERN"
+    case casual = "CASUAL"
+    case street = "STREET"
+    case lovely = "LOVELY"
+    case hip = "HIP"
+    case luxury = "LUXURY"
     
     public var styleTagKoreanString: String {
         switch self {
-        case .minimal:
-            return "미니멀"
-        case .modern:
-            return "모던"
-        case .casual:
-            return "캐주얼"
-        case .street:
-            return "스트릿"
-        case .lovely:
-            return "러블리"
-        case .hip:
-            return "힙"
-        case .luxury:
-            return "럭셔리"
+        case .minimal: return "미니멀"
+        case .modern: return "모던"
+        case .casual: return "캐주얼"
+        case .street: return "스트릿"
+        case .lovely: return "러블리"
+        case .hip: return "힙"
+        case .luxury: return "럭셔리"
         }
     }
     
