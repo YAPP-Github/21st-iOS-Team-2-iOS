23770e8b348a11b2e0594bfdf247faa7b04a37d6
Ethan <>
2023-02-11 08:39:24 +0900

    ✨ [feat] 온보딩 - 닉네임 뷰 및 로직 작업

--- Fitfty/Projects/Core/Sources/Repositories/OnboardingRepository.swift
+++ Fitfty/Projects/Core/Sources/Repositories/OnboardingRepository.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+public protocol OnboardingRepository {
+    
+    func checkNickname(_ nickname: String) async throws -> Bool
+}
+
+public final class DefaultOnboardingRepository: OnboardingRepository {
+    public init() {}
+    
+    public func checkNickname(_ nickname: String) async throws -> Bool {
+        let response = try await FitftyAPI.request(target: .checkNickname(query: nickname), dataType: CheckNicknameResponse.self)
+        return !response.data
+    }
+}
--- Fitfty/Projects/Onboarding/Sources/Nickname/NicknameViewModel.swift
+++ Fitfty/Projects/Onboarding/Sources/Nickname/NicknameViewModel.swift
@@ -7,3 +7,74 @@
 //
 
 import Foundation
+import Combine
+
+import Common
+import Core
+
+final public class NicknameViewModel: ViewModelType {
+    public enum ViewModelState {
+        case changeNextButtonState(isEnabled: Bool)
+        case pushGenderView
+    }
+    
+    private let repository: OnboardingRepository
+    
+    public var state: AnyPublisher<ViewModelState, Never> { currentState.compactMap { $0 }.eraseToAnyPublisher() }
+    private var currentState: CurrentValueSubject<ViewModelState?, Never> = .init(nil)
+    
+    @Published var hasAvailableString = false
+    @Published var hasNoOverlappingNickname = false
+    private var nickname = ""
+    private var isEnabledNextButton: Bool {
+        return hasAvailableString && hasNoOverlappingNickname
+    }
+    
+    public init(repository: OnboardingRepository) {
+        self.repository = repository
+    }
+    
+    func checkNicknameAvailable(_ nickname: String) {
+        hasAvailableString(nickname)
+    }
+    
+    func didTapNextButton() {
+        UserDefaults.standard.write(key: .nickname, value: nickname)
+        currentState.send(.pushGenderView)
+    }
+}
+
+private extension NicknameViewModel {
+    private func hasAvailableString(_ nickname: String) {
+        self.nickname = nickname
+        let regex = "^[0-9a-zA-Z]{6,30}$"
+        if nickname.range(of: regex, options: .regularExpression) != nil {
+            hasAvailableString = true
+            hasNoOverlappingNickname(nickname)
+        } else {
+            hasAvailableString = false
+            hasNoOverlappingNickname = false
+            checkEnabledNextButton()
+        }
+    }
+    
+    private func hasNoOverlappingNickname(_ nickname: String) {
+        Task {
+            do {
+                let result = try await repository.checkNickname(nickname)
+                hasNoOverlappingNickname = result ? true : false
+                checkEnabledNextButton()
+            } catch {
+                hasNoOverlappingNickname = false
+            }
+        }
+    }
+    
+    private func checkEnabledNextButton() {
+        if isEnabledNextButton {
+            currentState.send(.changeNextButtonState(isEnabled: true))
+        } else {
+            currentState.send(.changeNextButtonState(isEnabled: false))
+        }
+    }
+}
