e53ca2d0f8a5c949d465e4e8c8cc072c1343f2a4
Ethan <>
2023-02-11 15:17:56 +0900

    ✨ [feat] OnboardingError 추가

--- Fitfty/Projects/Core/Sources/Network/OnboardingError.swift
+++ Fitfty/Projects/Core/Sources/Network/OnboardingError.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+public enum OnboardingError: Error {
+    case noUserData
+    case others(String)
+}
+
+extension OnboardingError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .noUserData:
+            return NSLocalizedString("유저 정보가 없어요. 로그인을 다시 진행해주세요", comment: "No User Data")
+        case .others(let message):
+            return NSLocalizedString(message, comment: "Others Message")
+        }
+    }
+}
