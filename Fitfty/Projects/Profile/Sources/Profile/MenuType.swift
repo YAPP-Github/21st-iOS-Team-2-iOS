48d0fbe688a41156be2e84b5daa92255c726549c
youngsun <dudtjs1021@hansung.ac.kr>
2023-02-12 19:08:50 +0900

    ✨ [feat] 메뉴에 탭 내 핏프티 목록 조회, 북마크 목록 조회 구현
    - MenuType 추가

---
+++ Fitfty/Projects/Profile/Sources/Profile/MenuType.swift
@@ -0,0 +1,14 @@
+//
+//  MenuType.swift
+//  Profile
+//
+//  Created by 임영선 on 2023/02/12.
+//  Copyright © 2023 Fitfty. All rights reserved.
+//
+
+import Foundation
+
+enum MenuType {
+    case myFitfty
+    case bookmark
+}
