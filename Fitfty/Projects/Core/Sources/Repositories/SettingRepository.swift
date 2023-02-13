671cf26654af9e927d245a9315d7f807d43a7a65
EthanPark <ethanpark604@gmail.com>
2023-02-14 01:29:20 +0900

    ✨ [feat] Fitfty API 추가 및 SettingRepository 작업

--- Fitfty/Projects/Core/Sources/Repositories/SettingRepository.swift
+++ Fitfty/Projects/Core/Sources/Repositories/SettingRepository.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+public protocol SettingRepository {
+    func getUserPrivacy() async throws -> UserPrivacyResponse
+    func updateUserPrivacy(nickname: String, birthday: String, gender: String) async throws
+    func getUserProfile() async throws -> SettingUserDetailResponse
+    func updateUserProfile() async throws
+    func withdrawAccount() async throws -> Bool
+    func logout()
+}
+
+public final class DefaultSettingRepository: SettingRepository {
+    public init() {}
+    
+    public func getUserPrivacy() async throws -> UserPrivacyResponse {
+        return try await FitftyAPI.request(target: .getUserPrivacy,
+                                           dataType: UserPrivacyResponse.self)
+    }
+    
+    public func updateUserPrivacy(nickname: String, birthday: String, gender: String) async throws {
+        let parameters = try UserPrivacyRequest(nickname: nickname,
+                                                birthday: birthday,
+                                                gender: gender).asDictionary()
+        
+        let response = try await FitftyAPI.request(
+            target: .updateUserPrivacy(parameters: parameters),
+            dataType: UserPrivacyResponse.self
+        )
+    }
+    
+    public func getUserProfile() async throws -> SettingUserDetailResponse {
+        return try await FitftyAPI.request(target: .getMyProfile,
+                                           dataType: SettingUserDetailResponse.self)
+    }
+    
+    public func updateUserProfile() async throws {
+        
+    }
+    
+    public func withdrawAccount() async throws -> Bool {
+        let resposne = try await FitftyAPI.request(target: .withdrawAccount,
+                                                   dataType: WithdrawAccountResponse.self)
+        return resposne.result == "SUCCESS" ? true : false
+    }
+
+    public func logout() {
+        Keychain.deleteTokenData()
+    }
+}
