f86560b47599ac2801bd09319fab84bdb3b7cdb6
EthanPark <ethanpark604@gmail.com>
2023-02-14 04:43:09 +0900

    ✨ [feat] 계정 탈퇴 뷰 및 로직 추가

--- Fitfty/Projects/Setting/Sources/Withdraw/WithdrawImageCell.swift
+++ Fitfty/Projects/Setting/Sources/Withdraw/WithdrawImageCell.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2023 Fitfty. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+import Common
+
+final class WithdrawImageCell: UICollectionViewCell {
+    private let imageView = UIImageView()
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 221, height: 252)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    func setUp(item: Int) {
+        switch item {
+        case 0: imageView.image = CommonAsset.Images.introImage1.image
+        case 1: imageView.image = CommonAsset.Images.introImage2.image
+        case 2: imageView.image = CommonAsset.Images.introImage3.image
+        default: imageView.image = CommonAsset.Images.introImage1.image
+        }
+    }
+
+    private func configure() {
+        contentView.addSubviews(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+    }
+}
--- Fitfty/Projects/Setting/Sources/Withdraw/WithdrawViewModel.swift
+++ Fitfty/Projects/Setting/Sources/Withdraw/WithdrawViewModel.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import Combine
+
+import Common
+import Core
+
+final public class WithdrawViewModel: ViewModelType {
+    public enum ViewModelState {
+        case pushWithdrawConfirmView
+        case showErrorAlert(_ error: Error)
+    }
+    
+    private let repository: SettingRepository
+
+    public var state: AnyPublisher<ViewModelState, Never> { currentState.compactMap { $0 }.eraseToAnyPublisher() }
+    private var currentState: CurrentValueSubject<ViewModelState?, Never> = .init(nil)
+    
+    public init(repository: SettingRepository) {
+        self.repository = repository
+    }
+    
+    func didTapWithdrawButton() {
+        Task {
+            do {
+                let response = try await repository.withdrawAccount()
+                if response {
+                    repository.logout()
+                    currentState.send(.pushWithdrawConfirmView)
+                } else {
+                    currentState.send(.showErrorAlert(SettingError.failWithdrawAccount))
+                }
+            } catch {
+                currentState.send(.showErrorAlert(error))
+            }
+        }
+    }
+}
