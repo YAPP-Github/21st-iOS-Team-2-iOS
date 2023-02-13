9955780c26f110b29c0e5fe9de00e984ae7db45a
EthanPark <ethanpark604@gmail.com>
2023-02-14 01:28:05 +0900

    ✨ [feat] Setting Error 추가

--- Fitfty/Projects/Core/Sources/Error/SettingError.swift
+++ Fitfty/Projects/Core/Sources/Error/SettingError.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+public enum SettingError: Error {
+    case noNickname
+    case noBirthday
+    case noAvailableBirthday
+    case noAvailableNickname
+    case overlappedNickname
+    case noToken
+    case others(String)
+}
+
+extension SettingError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .noNickname:
+            return NSLocalizedString("닉네임을 입력해주세요", comment: "No Nickname")
+        case .noBirthday:
+            return NSLocalizedString("생년월일을 입력해주세요", comment: "No Birthday")
+        case .noAvailableBirthday:
+            return NSLocalizedString("올바른 생년월일을 기입해주세요", comment: "Wrong Birthday")
+        case .noAvailableNickname:
+            return NSLocalizedString("6자 이상의 영문과 숫자조합으로 이루어진 닉네임을 기입해주세요", comment: "Wrond Nickname")
+        case .overlappedNickname:
+            return NSLocalizedString("중복된 닉네임이에요", comment: "Overlapped Nickname")
+        case .noToken:
+            return NSLocalizedString("토큰이 없습니다", comment: "No Token")
+        case .others(let message):
+            return NSLocalizedString(message, comment: "Others Message")
+        }
+    }
+}
