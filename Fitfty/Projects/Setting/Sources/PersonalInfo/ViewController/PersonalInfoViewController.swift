1448238f3be8ff0e1cbb7fd796d0ca4cf18e2f1b
EthanPark <ethanpark604@gmail.com>
2023-02-14 01:32:53 +0900

    ✨ [feat] 개인정보 설정 뷰 및 로직 작업

--- Fitfty/Projects/Setting/Sources/PersonalInfo/ViewController/PersonalInfoViewController.swift
+++ Fitfty/Projects/Setting/Sources/PersonalInfo/ViewController/PersonalInfoViewController.swift
@@ -7,28 +7,38 @@
 //
 
 import UIKit
+import Combine
+
 import Common
 
 public final class PersonalInfoViewController: UIViewController {
     
     private weak var coordinator: PersonalInfoCoordinatorInterface?
     private var viewModel: PersonalInfoViewModel
+    private var cancellables = Set<AnyCancellable>()
     
     private var dataSource: UICollectionViewDiffableDataSource<PersonalInfoSection, Setting>?
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        setUp()
     }
     
     public init(coordinator: PersonalInfoCoordinatorInterface, viewModel: PersonalInfoViewModel) {
         self.coordinator = coordinator
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+        setUp()
+        bind()
+        
+        viewModel.getUserPrivacy()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     private lazy var saveButton: UIButton = {
@@ -48,7 +58,9 @@ public final class PersonalInfoViewController: UIViewController {
     private lazy var collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
         collectionView.register(PersonalInfoInputCell.self)
+        collectionView.register(FeedSettingCell.self)
         collectionView.register(SettingCell.self)
+        collectionView.register(FeedSettingHeaderView.self, forSupplementaryViewOfKind: FeedSettingHeaderView.className)
         collectionView.register(FooterView.self, forSupplementaryViewOfKind: FooterView.className)
         collectionView.backgroundColor = .white
         collectionView.delegate = self
@@ -56,26 +68,29 @@ public final class PersonalInfoViewController: UIViewController {
         return collectionView
     }()
     
-    private lazy var contactTextField: FitftyTextField = {
-        let textFiled = FitftyTextField(style: .normal, placeHolderText: "010-1234-5678")
+    private lazy var birthdayTextField: FitftyTextField = {
+        let textFiled = FitftyTextField(style: .normal, placeHolderText: "YYMMDD")
         textFiled.font = FitftyFont.appleSDSemiBold(size: 16).font
+        textFiled.delegate = self
         return textFiled
     }()
     
-    private lazy var dateTextField: FitftyTextField = {
-        let textFiled = FitftyTextField(style: .normal, placeHolderText: "YYMMDD")
+    private lazy var nicknameTextField: FitftyTextField = {
+        let textFiled = FitftyTextField(style: .normal, placeHolderText: "6자 이상의 영문 혹은 영문과 숫자를 조합")
         textFiled.font = FitftyFont.appleSDSemiBold(size: 16).font
+        textFiled.delegate = self
         return textFiled
     }()
     
     private lazy var emailTextField: FitftyTextField = {
-        let textFiled = FitftyTextField(style: .normal, placeHolderText: "fitfty@gmail.com")
+        let textFiled = FitftyTextField(style: .disabled, placeHolderText: "fitfty@gmail.com")
         textFiled.font = FitftyFont.appleSDSemiBold(size: 16).font
+        textFiled.delegate = self
         return textFiled
     }()
     
     private lazy var textfieldList: [FitftyTextField] = {
-        return [contactTextField, dateTextField, emailTextField]
+        return [birthdayTextField, nicknameTextField, emailTextField]
     }()
     
 }
@@ -87,6 +102,33 @@ private extension PersonalInfoViewController {
         setUpLayout()
         setUpDataSource()
         applySnapshot()
+    }
+    
+    func bind() {
+        viewModel.state
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] state in
+                switch state {
+                case .updateUserPrivacy(let userPrivacy):
+                    self?.nicknameTextField.text = userPrivacy.nickname
+                    self?.birthdayTextField.text = userPrivacy.birtyday
+                    self?.emailTextField.text = userPrivacy.email
+                    self?.collectionView.reloadData()
+                    
+                case .showAuthView:
+                    self?.coordinator?.showAuthView()
+                    
+                case .pushWithdrawAccountView:
+                    break
+                    
+                case .popView:
+                    self?.coordinator?.finished()
+                    
+                case .showErrorAlert(let error):
+                    self?.showAlert(message: error.localizedDescription)
+                }
+            }
+            .store(in: &cancellables)
     }
     
     func setUpNavigationBar() {
@@ -110,7 +152,8 @@ private extension PersonalInfoViewController {
     }
     
     @objc func didTapSaveButton(_ sender: UITapGestureRecognizer) {
-        coordinator?.finished()
+        viewModel.didTapSaveButton(nickname: nicknameTextField.text,
+                                   birthday: birthdayTextField.text)
     }
     
     func setUpLayout() {
@@ -129,6 +172,7 @@ private extension PersonalInfoViewController {
             let section = PersonalInfoSection(index: sectionNumber)
             switch section {
             case .info: return self?.infoListLayout()
+            case .gender: return self?.genderLayout()
             case .etc: return self?.etcLayout()
             default: return nil
             }
@@ -148,9 +192,38 @@ private extension PersonalInfoViewController {
             subitems: [.init(layoutSize: layoutSize)]
         )
         let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = .init(top: 20, leading: 20, bottom: 60, trailing: 20)
+        section.contentInsets = .init(top: 20, leading: 20, bottom: 30, trailing: 20)
         section.interGroupSpacing = 30
+        
+        return section
+    }
+    
+    func genderLayout() -> NSCollectionLayoutSection? {
+        let layoutSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(86),
+            heightDimension: .absolute(43)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: .init(
+                widthDimension: layoutSize.widthDimension,
+                heightDimension: layoutSize.heightDimension
+            ),
+            subitems: [.init(layoutSize: layoutSize)]
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 8, leading: 20, bottom: 28, trailing: 20)
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = 8
+
         section.boundarySupplementaryItems = [
+            NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .estimated(22)
+                ),
+                elementKind: FeedSettingHeaderView.className, alignment: .top
+            ),
             NSCollectionLayoutBoundarySupplementaryItem(
                 layoutSize: .init(
                     widthDimension: .absolute(view.safeAreaLayoutGuide.layoutFrame.width),
@@ -158,6 +231,7 @@ private extension PersonalInfoViewController {
                 ),
                 elementKind: FooterView.className, alignment: .bottom)
         ]
+        
         return section
     }
     
@@ -196,16 +270,35 @@ private extension PersonalInfoViewController {
                     cell.setUp(title: item.title, textField: textField)
                     return cell
                     
+                case .gender:
+                    guard let cell = collectionView.dequeueReusableCell(
+                        FeedSettingCell.self,
+                        for: indexPath) else {
+                        return UICollectionViewCell()
+                    }
+                    
+                    if indexPath.item == 0 {
+                        let isSelected = self?.viewModel.isFemaleUser() == true
+                        cell.setUp(title: item.title, isSelected: isSelected)
+                    } else {
+                        let isSelected = self?.viewModel.isMaleUser() == true
+                        cell.setUp(title: item.title, isSelected: isSelected)
+                    }
+                    
+                    return cell
+                    
                 case .etc:
                     guard let cell = collectionView.dequeueReusableCell(SettingCell.self, for: indexPath) else {
                         return UICollectionViewCell()
                     }
                     cell.setUp(item: item)
                     return cell
+                    
                 default: return UICollectionViewCell()
                 }
                 
             })
+        
         dataSource?.supplementaryViewProvider = { collectionView, elementKind, indexPath in
             switch elementKind {
             case FooterView.className:
@@ -214,6 +307,16 @@ private extension PersonalInfoViewController {
                     withReuseIdentifier: FooterView.className,
                     for: indexPath
                 )
+                
+            case FeedSettingHeaderView.className:
+                let headerView = collectionView.dequeueReusableSupplementaryView(
+                    ofKind: elementKind,
+                    withReuseIdentifier: FeedSettingHeaderView.className,
+                    for: indexPath
+                ) as? FeedSettingHeaderView
+                headerView?.setUp(title: Setting.gender.title)
+                return headerView
+                
             default: return UICollectionReusableView()
             }
         }
@@ -224,27 +327,49 @@ private extension PersonalInfoViewController {
         var snapshot = NSDiffableDataSourceSnapshot<PersonalInfoSection, Setting>()
         snapshot.appendSections([.info])
         snapshot.appendItems(Setting.info())
+        snapshot.appendSections([.gender])
+        snapshot.appendItems(Setting.gender())
         snapshot.appendSections([.etc])
         snapshot.appendItems(Setting.etc())
         dataSource?.apply(snapshot)
     }
+    
+    private func confirmLogoutAlert(completion: @escaping () -> Void) {
+        let alert = UIAlertController(title: "알림", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        alert.addAction(UIAlertAction(title: "확인", style: .default) { _ in
+            completion()
+        })
+        self.present(alert, animated: true)
+    }
 }
 
 extension PersonalInfoViewController: UICollectionViewDelegate {
-    
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let item = dataSource?.itemIdentifier(for: indexPath) else {
             return
         }
         switch item {
+        case .male, .female:
+            let cell = collectionView.cellForItem(at: indexPath) as? FeedSettingCell
+            viewModel.didTapGender(item: indexPath.item)
+            
         case .logout:
-            print(item)
+            confirmLogoutAlert { [weak self] in
+                self?.viewModel.logout()
+            }
             
         case .membershipWithdrawal:
-            print(item)
+            viewModel.withdrawAccount()
             
         default: break
         }
     }
-    
+}
+
+extension PersonalInfoViewController: UITextFieldDelegate {
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
--- Fitfty/Projects/Setting/Sources/PersonalInfo/ViewModel/PersonalInfoViewModel.swift
+++ Fitfty/Projects/Setting/Sources/PersonalInfo/ViewModel/PersonalInfoViewModel.swift
@@ -7,32 +7,159 @@
 //
 
 import Foundation
-import Common
 import Combine
+
+import Common
+import Core
 
 enum PersonalInfoSection {
     case info
+    case gender
     case etc
     
     init?(index: Int) {
         switch index {
         case 0: self = .info
-        case 1: self = .etc
+        case 1: self = .gender
+        case 2: self = .etc
         default: return nil
         }
     }
 }
 
-public final class PersonalInfoViewModel {
-
-    private var currentState: CurrentValueSubject<ViewModelState?, Never> = .init(nil)
-
-    public init() {}
-
-}
-
-extension PersonalInfoViewModel: ViewModelType {
-    public enum ViewModelState {}
+public final class PersonalInfoViewModel: ViewModelType {
+    public enum ViewModelState {
+        case updateUserPrivacy(userPrivacy: UserPrivacy)
+        case pushWithdrawAccountView
+        case showAuthView
+        case popView
+        case showErrorAlert(_ error: Error)
+    }
+    
+    private let repository: SettingRepository
     
     public var state: AnyPublisher<ViewModelState, Never> { currentState.compactMap { $0 }.eraseToAnyPublisher() }
+    private var currentState: CurrentValueSubject<ViewModelState?, Never> = .init(nil)
+    
+    private var userPrivacy = UserPrivacy(email: "")
+    private var hasAvailableNickname = false
+    private var hasAvailableBirthday = false
+    
+    public init(repository: SettingRepository) {
+        self.repository = repository
+    }
+    
+    func didTapSaveButton(nickname: String?, birthday: String?) {
+        guard let nickname = nickname else {
+            currentState.send(.showErrorAlert(SettingError.noNickname))
+            return
+        }
+        
+        guard let birthday = birthday else {
+            currentState.send(.showErrorAlert(SettingError.noBirthday))
+            return
+        }
+        
+        checkAvailableNickname(nickname)
+        checkAvailableBirthday(birthday)
+        
+        guard hasAvailableNickname else {
+            currentState.send(.showErrorAlert(SettingError.noAvailableNickname))
+            return
+        }
+
+        guard hasAvailableBirthday else {
+            currentState.send(.showErrorAlert(SettingError.noAvailableBirthday))
+            return
+        }
+        
+        userPrivacy.nickname = nickname
+        userPrivacy.birtyday = birthday
+        
+        saveNewUserPrivacy()
+    }
+    
+    func getUserPrivacy() {
+        Task {
+            do {
+                let response = try await repository.getUserPrivacy()
+                
+                userPrivacy = UserPrivacy(email: response.data?.email,
+                                          nickname: response.data?.nickname,
+                                          birtyday: response.data?.birthday,
+                                          gender: response.data?.gender)
+                currentState.send(.updateUserPrivacy(userPrivacy: userPrivacy))
+            } catch {
+                currentState.send(.showErrorAlert(error))
+            }
+        }
+    }
+    
+    func didTapGender(item: Int) {
+        if item == 0 {
+            userPrivacy.gender = "FEMALE"
+        } else {
+            userPrivacy.gender = "MALE"
+        }
+        
+        currentState.send(.updateUserPrivacy(userPrivacy: userPrivacy))
+    }
+    
+    func withdrawAccount() {
+        currentState.send(.pushWithdrawAccountView)
+    }
+    
+    func logout() {
+        repository.logout()
+        currentState.send(.showAuthView)
+    }
+    
+    func isFemaleUser() -> Bool {
+        return userPrivacy.gender == "FEMALE" ? true : false
+    }
+    
+    func isMaleUser() -> Bool {
+        return userPrivacy.gender == "MALE" ? true : false
+    }
+}
+
+private extension PersonalInfoViewModel {
+    func saveNewUserPrivacy() {
+        guard let nickname = userPrivacy.nickname,
+              let birthday = userPrivacy.birtyday,
+              let gender = userPrivacy.gender else {
+            return
+        }
+        
+        Task {
+            do {
+                try await repository.updateUserPrivacy(nickname: nickname,
+                                                       birthday: birthday,
+                                                       gender: gender)
+                currentState.send(.popView)
+            } catch {
+                currentState.send(.showErrorAlert(error))
+            }
+        }
+    }
+    
+    func checkAvailableNickname(_ nickname: String) {
+        self.userPrivacy.nickname = nickname
+        let regex = "^[0-9a-zA-Z]{6,30}$"
+        if nickname.range(of: regex, options: .regularExpression) != nil {
+            hasAvailableNickname = true
+        } else {
+            hasAvailableNickname = false
+        }
+    }
+    
+    func checkAvailableBirthday(_ birthday: String) {
+        self.userPrivacy.birtyday = birthday
+        let regex = "^[0-9]{6}$"
+        if birthday.range(of: regex, options: .regularExpression) != nil {
+            hasAvailableBirthday = true
+        } else {
+            hasAvailableBirthday = false
+        }
+    }
 }
