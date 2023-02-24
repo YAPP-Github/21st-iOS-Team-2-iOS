33c95903e3979004ea7746fdd68e11e84a572f02
EthanPark <ethanpark604@gmail.com>
2023-02-24 17:36:28 +0900

    ✨ [feat] LinksTextView 추가

--- Fitfty/Projects/Common/Sources/LinksTextView.swift
+++ Fitfty/Projects/Common/Sources/LinksTextView.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2023 Fitfty. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public final class LinksTextView: UITextView, UITextViewDelegate {
+    public typealias Links = [String: String]
+    public typealias OnLinkTap = (URL) -> Bool
+    
+    public var onLinkTap: OnLinkTap?
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        isEditable = false
+        isSelectable = true
+        isScrollEnabled = false
+        delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    public func addLinks(_ links: Links) {
+        guard attributedText.length > 0  else {
+            return
+        }
+        let mText = NSMutableAttributedString(attributedString: attributedText)
+        
+        for (linkText, urlString) in links {
+            if linkText.count > 0 {
+                let linkRange = mText.mutableString.range(of: linkText)
+                mText.addAttribute(.link, value: urlString, range: linkRange)
+                mText.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: linkRange)
+            }
+        }
+        attributedText = mText
+    }
+    
+    public func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange) -> Bool {
+        return onLinkTap?(URL) ?? true
+    }
+    
+    public func textViewDidChangeSelection(_ textView: UITextView) {
+        textView.selectedTextRange = nil
+    }
+}
